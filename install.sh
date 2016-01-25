e85d9aad5b2dac12616760dbe3adf24a9b7274c8
Kimmo Lehto <kimmo.lehto@inuit.fi>
2016-01-25 15:01:47 +0200

    Added tmux conf

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 CWD=`pwd`
-ln -s $CWD/.vim/vimrc $HOME/.vimrc
-ln -s $CWD/.oh-my-zsh/zshrc $HOME/.zshrc
+ln -s $CWD/.vim/vimrc $HOME/.vimrc || echo ".vimrc already exists?"
+ln -s $CWD/.oh-my-zsh/zshrc $HOME/.zshrc || echo ".zshrc already exists?"
+ln -s $CWD/.tmux/tmux.conf $HOME/.tmux.conf || echo ".tmux.conf already exists?"
+
